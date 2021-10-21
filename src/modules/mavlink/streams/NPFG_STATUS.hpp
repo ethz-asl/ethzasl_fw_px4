9aa3a412053d26bab47e7b30a88bd4f225f9c5bb
JaeyoungLim <jalim@ethz.ch>
2021-10-21 18:38:30 +0200

    Revert "fix publish wind est valid"
    This reverts commit 493bae1e096cd0906c6dffb80b91e9930e4c3df1.

--- src/modules/mavlink/streams/NPFG_STATUS.hpp
+++ src/modules/mavlink/streams/NPFG_STATUS.hpp
@@ -69,7 +69,6 @@ private:
 			mavlink_npfg_status_t msg{};
 
 			msg.timestamp = npfg_status.timestamp;
-			msg.wind_est_valid = npfg_status.wind_est_valid;
 			msg.lat_accel = npfg_status.lat_accel;
 			msg.lat_accel_ff = npfg_status.lat_accel_ff;
 			msg.bearing_feas = npfg_status.bearing_feas;
