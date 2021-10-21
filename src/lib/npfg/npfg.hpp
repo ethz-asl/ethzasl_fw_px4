14be0ba04cbbeb088b313047dc82286e2ef5f46c
JaeyoungLim <jalim@ethz.ch>
2021-10-21 18:38:30 +0200

    Revert "npfg: create const adaptPeriod method and update control params outside"
    This reverts commit e9b0a3eb6604260eb1787f631990ce5702f72863.

--- src/lib/npfg/npfg.hpp
+++ src/lib/npfg/npfg.hpp
@@ -409,8 +409,9 @@ private:
 		      const matrix::Vector2f &unit_path_tangent, const float signed_track_error, const float path_curvature);
 
 	/*
-	 * Adapts the controller period considering user defined inputs, current flight
-	* condition, path properties, and stability bounds.
+	 * Updates the proportional gain and time constant of the controller considering
+	 * user defined inputs, current flight condition, path properties, and stability
+	 * bounds.
 	 *
 	 * @param[in] ground_speed Vehicle ground speed [m/s]
 	 * @param[in] airspeed Vehicle airspeed [m/s]
@@ -420,12 +421,11 @@ private:
 	 * @param[in] wind_vel Wind velocity vector in inertial frame [m/s]
 	 * @param[in] unit_path_tangent Unit vector tangent to path at closest point
 	 *            in direction of path
-	* @param[in] feas_on_track Bearing feasibility on track at the closest point
-	* @return Adapted period [s]
 	 */
-	float adaptPeriod(const float ground_speed, const float airspeed, const float wind_ratio,
-			  const float track_error, const float path_curvature, const matrix::Vector2f &wind_vel,
-			  const matrix::Vector2f &unit_path_tangent, const float feas_on_track) const;
+	void updateControlParams(const float ground_speed, const float airspeed,
+				 const float wind_ratio, const float track_error, const float path_curvature,
+				 const matrix::Vector2f &wind_vel, const matrix::Vector2f &unit_path_tangent,
+				 const float feas_on_track);
 
 	/*
 	 * Returns normalized (unitless) and constrained track error [0,1].
